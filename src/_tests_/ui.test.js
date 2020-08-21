d646ee2fd40bd7b2fd12631f1ce8358ad7a51a9c
Michael Srithapin <michael.srithapin@gmail.com>
2020-08-20 20:54:32 -0500

    pushing react setup

---
+++ src/_tests_/ui.test.js
@@ -0,0 +1,12 @@
+// import dependencies
+import React from "react";
+// import react-testing methods
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
+// add custom jest matchers from jest-dom
+import '@testing-library/jest-dom/extend-expect';
+import App from "../App";
+
+describe('App', () => {
+
+
+});
