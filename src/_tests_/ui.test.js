1676711aaf06884e3053eb5a0f18a30f9bca4b98
Michael Srithapin <michael.srithapin@gmail.com>
2020-08-29 12:34:41 -0500

    test was written incorrectly, updating to test component

--- src/_tests_/ui.test.js
+++ src/_tests_/ui.test.js
@@ -4,8 +4,9 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 // add custom jest matchers from jest-dom
 import '@testing-library/jest-dom/extend-expect';
-import { configure } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from '../App'
 
 configure({ adapter: new Adapter() });
 
