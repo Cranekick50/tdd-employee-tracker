7c62be5d2669cfe8ee27afe638d8075208798752
Michael Srithapin <michael.srithapin@gmail.com>
2020-08-29 13:32:41 -0500

    App.js now renders component User.js

--- src/_tests_/ui.test.js
+++ src/_tests_/ui.test.js
@@ -8,6 +8,7 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../App'
 
+
 configure({ adapter: new Adapter() });
 
 describe('App', () => {
---
+++ src/component/Users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const User = () => {
+    console.log("this works")
+}
+
+export default User
